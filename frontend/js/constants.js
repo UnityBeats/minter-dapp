323dc7e8db9bd624c4457cf4dda2ab55ec537b2c
UnityBeats <36998010+UnityBeats@users.noreply.github.com>
2023-02-24 20:12:12 -0500

    asdf

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,6 @@
 const contractAddress = "0xf7568E5A5DA97Ee5D2ce91Ff4fE0488E4A04828b"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "The quick brown fox jumps over the lazy dog...";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
@@ -20,7 +17,3 @@ const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "FORGE YOUR KEYS";
 const mint_failed = "Minting failed. 😢 Please try again.";
-
-welcome_h1.classList.add('hidden');
-welcome_h2.classList.add('hidden');
-welcome_p.classList.add('hidden');
